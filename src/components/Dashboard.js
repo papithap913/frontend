f3ed2bf27331eab637f92d3f8768686cd5df3cb7
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-12-11 18:01:55 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axiosInstance from "..api/axiosInstance";
+import axiosInstance from "../api/axiosInstance";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {

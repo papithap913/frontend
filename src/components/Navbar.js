8517459d00d5fa81a2bb5c3b4cd636fbf1e0eb68
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-12-11 18:03:11 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
             ) : (
               // If the user is not logged in, show Login link
               <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link> 
+                <Link className="nav-link" to="/">Login</Link> 
                 <Link className="nav-link" to="/register">Register</Link>
               </li>
             )}

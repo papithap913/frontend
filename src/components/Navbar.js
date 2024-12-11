78b00dd217364b02faa345b60b394dedcc25d8a3
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-12-11 20:04:03 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,12 +17,16 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">Agent Portal</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/add-listing">Add Listing</Link>
-            </li>
+            {token && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/add-listing">Add Listing</Link>
+                </li>
+              </>
+            )}
           </ul>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             {token ? (
@@ -31,11 +35,15 @@ const Navbar = () => {
                 <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
               </li>
             ) : (
-              // If the user is not logged in, show Login link
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Login</Link> 
-                <Link className="nav-link" to="/register">Register</Link>
-              </li>
+              // If the user is not logged in, show Login and Register links
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
+                </li>
+              </>
             )}
           </ul>
         </div>
